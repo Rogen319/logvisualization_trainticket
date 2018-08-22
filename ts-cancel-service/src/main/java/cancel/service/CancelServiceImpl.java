00aab56dcb7562433f017f96dc09315427d4bfe9
kylinxiang <15884286060@163.com>
2018-08-22 14:37:57 +0800

    fix no header pass to RPC bug

--- ts-cancel-service/src/main/java/cancel/service/CancelServiceImpl.java
+++ ts-cancel-service/src/main/java/cancel/service/CancelServiceImpl.java
@@ -10,12 +10,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
 import java.text.DecimalFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 @Service
-public class CancelServiceImpl implements CancelService{
+public class CancelServiceImpl implements CancelService {
 
     @Autowired
     private RestTemplate restTemplate;
@@ -27,15 +28,15 @@ public class CancelServiceImpl implements CancelService{
     MockLog mockLog;
 
     @Override
-    public CancelOrderResult cancelOrder(CancelOrderInfo info,String loginToken,String loginId, HttpHeaders headers) throws Exception{
+    public CancelOrderResult cancelOrder(CancelOrderInfo info, String loginToken, String loginId, HttpHeaders headers) throws Exception {
         GetOrderByIdInfo getFromOrderInfo = new GetOrderByIdInfo();
         getFromOrderInfo.setOrderId(info.getOrderId());
         GetOrderResult orderResult = getOrderByIdFromOrder(getFromOrderInfo, headers);
-        if(orderResult.isStatus() == true){
+        if (orderResult.isStatus() == true) {
             mockLog.printLog("[Cancel Order Service][Cancel Order] Order found G|H");
             Order order = orderResult.getOrder();
-            if(order.getStatus() == OrderStatus.NOTPAID.getCode()
-                    || order.getStatus() == OrderStatus.PAID.getCode() || order.getStatus() == OrderStatus.CHANGE.getCode()){
+            if (order.getStatus() == OrderStatus.NOTPAID.getCode()
+                    || order.getStatus() == OrderStatus.PAID.getCode() || order.getStatus() == OrderStatus.CHANGE.getCode()) {
 
                 order.setStatus(OrderStatus.CANCEL.getCode());
                 ChangeOrderInfo changeOrderInfo = new ChangeOrderInfo();
@@ -43,21 +44,21 @@ public class CancelServiceImpl implements CancelService{
                 changeOrderInfo.setOrder(order);
 
                 ChangeOrderResult changeOrderResult = cancelFromOrder(changeOrderInfo, headers);
-                if(changeOrderResult.isStatus() == true){
+                if (changeOrderResult.isStatus() == true) {
                     CancelOrderResult finalResult = new CancelOrderResult();
                     finalResult.setStatus(true);
                     finalResult.setMessage("Success.");
                     mockLog.printLog("[Cancel Order Service][Cancel Order] Success.");
                     //Draw back money
                     String money = calculateRefund(order);
-                    boolean status = drawbackMoney(money,loginId, headers);
-                    if(status == true){
+                    boolean status = drawbackMoney(money, loginId, headers);
+                    if (status == true) {
                         mockLog.printLog("[Cancel Order Service][Draw Back Money] Success.");
 
                         GetAccountByIdInfo getAccountByIdInfo = new GetAccountByIdInfo();
                         getAccountByIdInfo.setAccountId(order.getAccountId().toString());
                         GetAccountByIdResult result = getAccount(getAccountByIdInfo, headers);
-                        if(result.isStatus() == false){
+                        if (result.isStatus() == false) {
                             return null;
                         }
 
@@ -77,14 +78,13 @@ public class CancelServiceImpl implements CancelService{
 
                         sendEmail(notifyInfo, headers);
 
-                    }else{
+                    } else {
                         mockLog.printLog("[Cancel Order Service][Draw Back Money] Fail.");
                     }
 
 
-
                     return finalResult;
-                }else{
+                } else {
                     CancelOrderResult finalResult = new CancelOrderResult();
                     finalResult.setStatus(false);
                     finalResult.setMessage(changeOrderResult.getMessage());
@@ -92,23 +92,23 @@ public class CancelServiceImpl implements CancelService{
                     return finalResult;
                 }
 
-            }else{
+            } else {
                 CancelOrderResult result = new CancelOrderResult();
                 result.setStatus(false);
                 result.setMessage("Order Status Cancel Not Permitted");
                 mockLog.printLog("[Cancel Order Service][Cancel Order] Order Status Not Permitted.");
                 return result;
             }
-        }else{
+        } else {
             GetOrderByIdInfo getFromOtherOrderInfo = new GetOrderByIdInfo();
             getFromOtherOrderInfo.setOrderId(info.getOrderId());
             GetOrderResult orderOtherResult = getOrderByIdFromOrderOther(getFromOtherOrderInfo, headers);
-            if(orderOtherResult.isStatus() == true){
+            if (orderOtherResult.isStatus() == true) {
                 mockLog.printLog("[Cancel Order Service][Cancel Order] Order found Z|K|Other");
 
                 Order order = orderOtherResult.getOrder();
-                if(order.getStatus() == OrderStatus.NOTPAID.getCode()
-                        || order.getStatus() == OrderStatus.PAID.getCode() || order.getStatus() == OrderStatus.CHANGE.getCode()){
+                if (order.getStatus() == OrderStatus.NOTPAID.getCode()
+                        || order.getStatus() == OrderStatus.PAID.getCode() || order.getStatus() == OrderStatus.CHANGE.getCode()) {
 
                     mockLog.printLog("[Cancel Order Service][Cancel Order] Order status ok");
 
@@ -195,35 +195,35 @@ public class CancelServiceImpl implements CancelService{
 //                    }
 
 //
-                    if(changeOrderResult.isStatus() == true){
+                    if (changeOrderResult.isStatus() == true) {
                         CancelOrderResult finalResult = new CancelOrderResult();
                         finalResult.setStatus(true);
                         finalResult.setMessage("Success.");
                         mockLog.printLog("[Cancel Order Service][Cancel Order] Success.");
                         //Draw back money
                         String money = calculateRefund(order);
-                        boolean status = drawbackMoney(money,loginId, headers);
-                        if(status == true){
+                        boolean status = drawbackMoney(money, loginId, headers);
+                        if (status == true) {
                             mockLog.printLog("[Cancel Order Service][Draw Back Money] Success.");
-                        }else{
+                        } else {
                             mockLog.printLog("[Cancel Order Service][Draw Back Money] Fail.");
                         }
                         return finalResult;
-                    }else{
+                    } else {
                         CancelOrderResult finalResult = new CancelOrderResult();
                         finalResult.setStatus(false);
                         finalResult.setMessage(changeOrderResult.getMessage());
                         mockLog.printLog("[Cancel Order Service][Cancel Order] Fail.Reason:" + changeOrderResult.getMessage());
                         return finalResult;
                     }
-                }else{
+                } else {
                     CancelOrderResult result = new CancelOrderResult();
                     result.setStatus(false);
                     result.setMessage("Order Status Cancel Not Permitted");
                     mockLog.printLog("[Cancel Order Service][Cancel Order] Order Status Not Permitted.");
                     return result;
                 }
-            }else{
+            } else {
                 CancelOrderResult result = new CancelOrderResult();
                 result.setStatus(false);
                 result.setMessage("Order Not Found");
@@ -233,7 +233,7 @@ public class CancelServiceImpl implements CancelService{
         }
     }
 
-    public boolean sendEmail(NotifyInfo notifyInfo, HttpHeaders headers ){
+    public boolean sendEmail(NotifyInfo notifyInfo, HttpHeaders headers) {
         mockLog.printLog("[Cancel Order Service][Send Email]");
         HttpEntity requestEntity = new HttpEntity(notifyInfo, headers);
         ResponseEntity<Boolean> re = restTemplate.exchange(
@@ -251,22 +251,22 @@ public class CancelServiceImpl implements CancelService{
     }
 
     @Override
-    public CalculateRefundResult calculateRefund(CancelOrderInfo info, HttpHeaders headers){
+    public CalculateRefundResult calculateRefund(CancelOrderInfo info, HttpHeaders headers) {
         GetOrderByIdInfo getFromOrderInfo = new GetOrderByIdInfo();
         getFromOrderInfo.setOrderId(info.getOrderId());
         GetOrderResult orderResult = getOrderByIdFromOrder(getFromOrderInfo, headers);
-        if(orderResult.isStatus() == true){
+        if (orderResult.isStatus() == true) {
             Order order = orderResult.getOrder();
-            if(order.getStatus() == OrderStatus.NOTPAID.getCode()
-                    || order.getStatus() == OrderStatus.PAID.getCode()){
-                if(order.getStatus() == OrderStatus.NOTPAID.getCode()){
+            if (order.getStatus() == OrderStatus.NOTPAID.getCode()
+                    || order.getStatus() == OrderStatus.PAID.getCode()) {
+                if (order.getStatus() == OrderStatus.NOTPAID.getCode()) {
                     CalculateRefundResult result = new CalculateRefundResult();
                     result.setStatus(true);
                     result.setMessage("Success");
                     result.setRefund("0");
                     mockLog.printLog("[Cancel Order][Refund Price] From Order Service.Not Paid.");
                     return result;
-                }else{
+                } else {
                     CalculateRefundResult result = new CalculateRefundResult();
                     result.setStatus(true);
                     result.setMessage("Success");
@@ -274,7 +274,7 @@ public class CancelServiceImpl implements CancelService{
                     mockLog.printLog("[Cancel Order][Refund Price] From Order Service.Paid.");
                     return result;
                 }
-            }else{
+            } else {
                 CalculateRefundResult result = new CalculateRefundResult();
                 result.setStatus(false);
                 result.setMessage("Order Status Cancel Not Permitted");
@@ -283,22 +283,22 @@ public class CancelServiceImpl implements CancelService{
 
                 return result;
             }
-        }else{
+        } else {
             GetOrderByIdInfo getFromOtherOrderInfo = new GetOrderByIdInfo();
             getFromOtherOrderInfo.setOrderId(info.getOrderId());
             GetOrderResult orderOtherResult = getOrderByIdFromOrderOther(getFromOtherOrderInfo, headers);
-            if(orderOtherResult.isStatus() == true){
+            if (orderOtherResult.isStatus() == true) {
                 Order order = orderOtherResult.getOrder();
-                if(order.getStatus() == OrderStatus.NOTPAID.getCode()
-                        || order.getStatus() == OrderStatus.PAID.getCode()){
-                    if(order.getStatus() == OrderStatus.NOTPAID.getCode()){
+                if (order.getStatus() == OrderStatus.NOTPAID.getCode()
+                        || order.getStatus() == OrderStatus.PAID.getCode()) {
+                    if (order.getStatus() == OrderStatus.NOTPAID.getCode()) {
                         CalculateRefundResult result = new CalculateRefundResult();
                         result.setStatus(true);
                         result.setMessage("Success");
                         result.setRefund("0");
                         mockLog.printLog("[Cancel Order][Refund Price] From Order Other Service.Not Paid.");
                         return result;
-                    }else{
+                    } else {
                         CalculateRefundResult result = new CalculateRefundResult();
                         result.setStatus(true);
                         result.setMessage("Success");
@@ -306,7 +306,7 @@ public class CancelServiceImpl implements CancelService{
                         mockLog.printLog("[Cancel Order][Refund Price] From Order Other Service.Paid.");
                         return result;
                     }
-                }else{
+                } else {
                     CalculateRefundResult result = new CalculateRefundResult();
                     result.setStatus(false);
                     result.setMessage("Order Status Cancel Not Permitted");
@@ -314,7 +314,7 @@ public class CancelServiceImpl implements CancelService{
                     mockLog.printLog("[Cancel Order][Refund Price] Order Other. Cancel Not Permitted.");
                     return result;
                 }
-            }else{
+            } else {
                 CalculateRefundResult result = new CalculateRefundResult();
                 result.setStatus(false);
                 result.setMessage("Order Not Found");
@@ -325,8 +325,8 @@ public class CancelServiceImpl implements CancelService{
         }
     }
 
-    private String calculateRefund(Order order){
-        if(order.getStatus() == OrderStatus.NOTPAID.getCode()){
+    private String calculateRefund(Order order) {
+        if (order.getStatus() == OrderStatus.NOTPAID.getCode()) {
             return "0.00";
         }
         mockLog.printLog("[Cancel Order] Order Travel Date:" + order.getTravelDate().toString());
@@ -342,17 +342,17 @@ public class CancelServiceImpl implements CancelService{
         int minute = cal2.get(Calendar.MINUTE);
         int second = cal2.get(Calendar.SECOND);
         Date startTime = new Date(year,
-                                  month,
-                                  day,
-                                  hour,
-                                  minute,
-                                  second);
+                month,
+                day,
+                hour,
+                minute,
+                second);
         mockLog.printLog("[Cancel Order] nowDate  :" + nowDate.toString());
         mockLog.printLog("[Cancel Order] startTime:" + startTime.toString());
-        if(nowDate.after(startTime)){
+        if (nowDate.after(startTime)) {
             mockLog.printLog("[Cancel Order] Ticket expire refund 0");
             return "0";
-        }else{
+        } else {
             double totalPrice = Double.parseDouble(order.getPrice());
             double price = totalPrice * 0.8;
             DecimalFormat priceFormat = new java.text.DecimalFormat("0.00");
@@ -363,13 +363,15 @@ public class CancelServiceImpl implements CancelService{
     }
 
 
-    private ChangeOrderResult cancelFromOrder(ChangeOrderInfo info,  HttpHeaders headers){
+    private ChangeOrderResult cancelFromOrder(ChangeOrderInfo info, HttpHeaders headers) {
         mockLog.printLog("[Cancel Order Service][Change Order Status] Changing....");
-        ChangeOrderResult result = restTemplate.postForObject("http://ts-order-service:12031/order/update",info,ChangeOrderResult.class);
+        HttpEntity<ChangeOrderInfo> request = new HttpEntity<>(info, headers);
+        ChangeOrderResult result = restTemplate.exchange("http://ts-order-service:12031/order/update",
+                HttpMethod.POST, request, ChangeOrderResult.class).getBody();
         return result;
     }
 
-    private ChangeOrderResult cancelFromOtherOrder(ChangeOrderInfo info, HttpHeaders headers){
+    private ChangeOrderResult cancelFromOtherOrder(ChangeOrderInfo info, HttpHeaders headers) {
         mockLog.printLog("[Cancel Order Service][Change Order Status] Changing....");
         HttpEntity requestEntity = new HttpEntity(info, headers);
         ResponseEntity<ChangeOrderResult> re = restTemplate.exchange(
@@ -382,7 +384,7 @@ public class CancelServiceImpl implements CancelService{
         return result;
     }
 
-    public boolean drawbackMoney(String money,String userId, HttpHeaders headers){
+    public boolean drawbackMoney(String money, String userId, HttpHeaders headers) {
         mockLog.printLog("[Cancel Order Service][Draw Back Money] Draw back money...");
         DrawBackInfo info = new DrawBackInfo();
         info.setMoney(money);
@@ -392,17 +394,17 @@ public class CancelServiceImpl implements CancelService{
                 "http://ts-inside-payment-service:18673/inside_payment/drawBack",
                 HttpMethod.POST,
                 requestEntity,
-               String.class);
+                String.class);
         String result = re.getBody();
 //        String result = restTemplate.postForObject("http://ts-inside-payment-service:18673/inside_payment/drawBack",info,String.class);
-        if(result.equals("true")){
+        if (result.equals("true")) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public GetAccountByIdResult getAccount(GetAccountByIdInfo info, HttpHeaders headers){
+    public GetAccountByIdResult getAccount(GetAccountByIdInfo info, HttpHeaders headers) {
         mockLog.printLog("[Cancel Order Service][Get By Id]");
         HttpEntity requestEntity = new HttpEntity(info, headers);
         ResponseEntity<GetAccountByIdResult> re = restTemplate.exchange(
@@ -419,7 +421,7 @@ public class CancelServiceImpl implements CancelService{
         return result;
     }
 
-    private GetOrderResult getOrderByIdFromOrder(GetOrderByIdInfo info, HttpHeaders headers){
+    private GetOrderResult getOrderByIdFromOrder(GetOrderByIdInfo info, HttpHeaders headers) {
         mockLog.printLog("[Cancel Order Service][Get Order] Getting....");
         HttpEntity requestEntity = new HttpEntity(info, headers);
         ResponseEntity<GetOrderResult> re = restTemplate.exchange(
@@ -434,7 +436,7 @@ public class CancelServiceImpl implements CancelService{
         return cor;
     }
 
-    private GetOrderResult getOrderByIdFromOrderOther(GetOrderByIdInfo info, HttpHeaders headers){
+    private GetOrderResult getOrderByIdFromOrderOther(GetOrderByIdInfo info, HttpHeaders headers) {
         mockLog.printLog("[Cancel Order Service][Get Order] Getting....");
         HttpEntity requestEntity = new HttpEntity(info, headers);
         ResponseEntity<GetOrderResult> re = restTemplate.exchange(
