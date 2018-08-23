ba7187eb40ac0d559a53704eec64b78202a037d1
kylinxiang <15884286060@163.com>
2018-08-23 15:15:05 +0800

    remove email config.

--- ts-notification-service/src/main/java/notification/controller/NotificationController.java
+++ ts-notification-service/src/main/java/notification/controller/NotificationController.java
@@ -1,7 +1,6 @@
 package notification.controller;
 
 import notification.domain.NotifyInfo;
-import notification.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
@@ -11,26 +10,24 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 public class NotificationController {
-    @Autowired
-    NotificationService service;
 
     @RequestMapping(value="/notification/preserve_success", method = RequestMethod.POST)
     public boolean preserve_success(@RequestBody NotifyInfo info, @RequestHeader HttpHeaders headers){
-        return service.preserve_success(info, headers);
+        return true;
     }
 
     @RequestMapping(value="/notification/order_create_success", method = RequestMethod.POST)
     public boolean order_create_success(@RequestBody NotifyInfo info, @RequestHeader HttpHeaders headers){
-        return service.order_create_success(info, headers);
+        return true;
     }
 
     @RequestMapping(value="/notification/order_changed_success", method = RequestMethod.POST)
     public boolean order_changed_success(@RequestBody NotifyInfo info, @RequestHeader HttpHeaders headers){
-        return service.order_changed_success(info, headers);
+        return true;
     }
 
     @RequestMapping(value="/notification/order_cancel_success", method = RequestMethod.POST)
     public boolean order_cancel_success(@RequestBody NotifyInfo info, @RequestHeader HttpHeaders headers){
-        return service.order_cancel_success(info, headers);
+        return true;
     }
 }
