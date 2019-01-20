b866a46ae1c5fadb44eb97489dcaba00160e3d06
lab401\lwh <2391289924@qq.com>
2019-01-20 14:48:06 +0800

    修改座位数量，以适应自动化测试需求

--- ts-train-service/src/main/java/train/init/InitData.java
+++ ts-train-service/src/main/java/train/init/InitData.java
@@ -20,38 +20,38 @@ public class InitData implements CommandLineRunner{
         Information info = new Information();
 
         info.setId("GaoTieOne");
-        info.setConfortClass(60);
-        info.setEconomyClass(120);
+        info.setConfortClass(60000);
+        info.setEconomyClass(120000);
         info.setAverageSpeed(250);
         service.create(info);
 
         info.setId("GaoTieTwo");
-        info.setConfortClass(80);
-        info.setEconomyClass(200);
+        info.setConfortClass(80000);
+        info.setEconomyClass(200000);
         info.setAverageSpeed(200);
         service.create(info);
 
         info.setId("DongCheOne");
-        info.setConfortClass(100);
-        info.setEconomyClass(300);
+        info.setConfortClass(100000);
+        info.setEconomyClass(300000);
         info.setAverageSpeed(180);
         service.create(info);
 
         info.setId("ZhiDa");
-        info.setConfortClass(60);
-        info.setEconomyClass(120);
+        info.setConfortClass(60000);
+        info.setEconomyClass(120000);
         info.setAverageSpeed(120);
         service.create(info);
 
         info.setId("TeKuai");
-        info.setConfortClass(80);
-        info.setEconomyClass(200);
+        info.setConfortClass(80000);
+        info.setEconomyClass(200000);
         info.setAverageSpeed(120);
         service.create(info);
 
         info.setId("KuaiSu");
-        info.setConfortClass(100);
-        info.setEconomyClass(300);
+        info.setConfortClass(100000);
+        info.setEconomyClass(300000);
         info.setAverageSpeed(90);
         service.create(info);
     }
